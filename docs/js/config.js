e1e8e939661bfb7f13f2693a404df0000ae61452
abhira0 <abhira0@protonmail.com>
2025-01-05 06:13:08 +0000

    Update API URL in configuration for production environment

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,5 +1,5 @@
 window.CONFIG = {
     API_URL: window.location.hostname === 'localhost' 
         ? 'http://localhost:3000/api'
-        : 'https://refactored-space-lamp-jgpvjwqggwvhq4wv-3000.app.github.dev/api'
+        : 'https://jobs.abhirao.in/api'
 };
