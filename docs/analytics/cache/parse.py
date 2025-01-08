41241cce7c187c362e51d563e6b87b990422ff8f
abhira0 <82208485+abhira0@users.noreply.github.com>
2025-01-08 06:47:13 +0000

    Refactor logging setup and improve code formatting for readability; add StatusEvents class to process job status updates

--- docs/analytics/cache/parse.py
+++ docs/analytics/cache/parse.py
@@ -6,7 +6,7 @@ from geopy.geocoders import Nominatim
 import time
 import threading
 import logging
-import coloredlogs
+import coloredlogs, collections
 
 # Create a logger object
 logger = logging.getLogger(__name__)
@@ -15,15 +15,19 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 # Create a colored formatter and add it to the logger
-coloredlogs.install(level='DEBUG', logger=logger,
+coloredlogs.install(
+    level="DEBUG",
+    logger=logger,
     fmt="%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s",
-        field_styles={
-        'asctime': {'color': 'green'},
-        'filename': {'color': 'magenta'},
-        'lineno': {'color': 'magenta'},
-        'levelname': {'color': 'cyan', 'bold': True},
-        'message': {'color': 'white'},
-    })
+    field_styles={
+        "asctime": {"color": "green"},
+        "filename": {"color": "magenta"},
+        "lineno": {"color": "magenta"},
+        "levelname": {"color": "cyan", "bold": True},
+        "message": {"color": "white"},
+    },
+)
+
 
 class JSONFile:
     def __init__(self, file_path: str, auto_save: bool = True):
@@ -34,15 +38,15 @@ class JSONFile:
 
     def load(self) -> Any:
         if not os.path.exists(self.file_path):
-            with open(self.file_path, 'w') as f:
+            with open(self.file_path, "w") as f:
                 json.dump([], f)
-        with open(self.file_path, 'r+') as f:
+        with open(self.file_path, "r+") as f:
             return json.load(f)
 
     def save(self, data: Any = []) -> None:
         if data:
             self.data = data
-        with open(self.file_path, 'w') as f:
+        with open(self.file_path, "w") as f:
             json.dump(data or self.data, f, indent=2)
         return self
 
@@ -51,36 +55,44 @@ class JSONFile:
             while True:
                 time.sleep(3)
                 self.save()
+
         thread = threading.Thread(target=auto_save, daemon=True)
         thread.start()
 
-LISTINGS_RAW = JSONFile('.github/scripts/listings.json', auto_save=False)
-LISTINGS_PARSED = JSONFile('docs/analytics/cache/listings_parsed.json')
-SIMPLIFY_TRACKER = JSONFile('docs/analytics/cache/simplify/raw.json', auto_save=False)
-SIMPLIFY_TRACKER_PARSED = JSONFile('docs/analytics/cache/simplify/parsed.json', auto_save=False)
-LOCATION_CACHE = JSONFile('docs/analytics/cache/locations.json')
 
-PREDEFINED_MAPPING = {"Austin, Texas Metropolitan Area": "Austin, Texas, USA"}
+LISTINGS_RAW = JSONFile(".github/scripts/listings.json", auto_save=False)
+LISTINGS_PARSED = JSONFile("docs/analytics/cache/listings_parsed.json")
+SIMPLIFY_TRACKER = JSONFile("docs/analytics/cache/simplify/raw.json", auto_save=False)
+SIMPLIFY_TRACKER_PARSED = JSONFile(
+    "docs/analytics/cache/simplify/parsed.json", auto_save=False
+)
+LOCATION_CACHE = JSONFile("docs/analytics/cache/locations.json")
+
 
 class FilterByDate:
-    def __init__(self, input, cutoff_date: datetime.datetime=datetime.datetime(2024, 1, 1)):
+    def __init__(
+        self, input, cutoff_date: datetime.datetime = datetime.datetime(2024, 1, 1)
+    ):
         self.input = input
         self.output = self.process(cutoff_date)
 
     def process(self, cutoff_date) -> None:
         entries = []
         for listing in self.input.data:
-            date = datetime.datetime.fromtimestamp(int(listing['date_posted']))
+            date = datetime.datetime.fromtimestamp(int(listing["date_posted"]))
             if date >= cutoff_date:
                 entries.append(listing)
         return LISTINGS_PARSED.save(entries)
+
 
 class AddCoordinates:
     def __init__(self, input):
         self.input = input
         self.output = self.process()
-    
-    def get_coord(self, location: str, location_cache: Dict, geolocator: Nominatim) -> List:
+
+    def get_coord(
+        self, location: str, location_cache: Dict, geolocator: Nominatim
+    ) -> List:
         location = location.strip()
         if "remote" in location.lower():
             location_cache[location] = ["remote", "remote", "remote"]
@@ -90,11 +102,11 @@ class AddCoordinates:
 
             loc = geolocator.geocode(location)
             assert loc, f"Location not found, {loc.raw}"
-            
+
             location_cache[location] = [loc.latitude, loc.longitude, loc.address]
         return location_cache[location]
-    
-    def get_all_locations(self, locations:str):
+
+    def get_all_locations(self, locations: str):
         cleaned_locations = []
         splitter = ["|", ";", "•", " and ", " or "]
         for spli in splitter:
@@ -115,30 +127,60 @@ class AddCoordinates:
         errors = []
 
         for item in self.input.data:
-            locations = self.get_all_locations(item['job_posting_location'])
+            locations = self.get_all_locations(item["job_posting_location"])
             if not locations:
                 logger.error(f"No locations found for {item['job_posting_location']}")
-            item['coordinates'] = []
+            item["coordinates"] = []
             for loc in locations:
                 try:
-                    item['coordinates'].append(self.get_coord(loc, location_cache, geolocator))
+                    item["coordinates"].append(
+                        self.get_coord(loc, location_cache, geolocator)
+                    )
                     # logger.info(f"Coordinates for {loc} is {item['coordinates']}")
                 except Exception as e:
                     errors.append(loc)
-                    logger.error(f"Error adding coordinates for {loc} : https://simplify.jobs/tracker?id={item['id']}")
-            
+                    logger.error(
+                        f"Error adding coordinates for {loc} : https://simplify.jobs/tracker?id={item['id']}"
+                    )
+
         LOCATION_CACHE.save(location_cache)
         return SIMPLIFY_TRACKER_PARSED.save(self.input.data)
+
+
+class StatusEvents:
+    def __init__(self, input):
+        self.input = input
+        self.output = self.process()
+
+    def process(self):
+        rejected = 0
+        print(
+            collections.Counter(
+                i["status"] for item in self.input.data for i in item["status_events"]
+            )
+        )
+        for item in self.input.data:
+            for status in item["status_events"]:
+                if status["status"] == 1:
+                    status["status"] = "saved"
+                elif status["status"] == 2:
+                    status["status"] = "applied"
+                elif status["status"] == 11:
+                    status["status"] = "screen"
+                elif status["status"] == 23:
+                    status["status"] = "rejected"
 
 
 def main():
     # Simplify Listings
     data = LISTINGS_RAW
     data = FilterByDate(data, cutoff_date=datetime.datetime(2024, 1, 1)).output
-    
+
     # Simplify Tracker Data
     data = SIMPLIFY_TRACKER
     data = AddCoordinates(data).output
-    
-if __name__ == '__main__':
+    data = StatusEvents(data).output
+
+
+if __name__ == "__main__":
     main()
